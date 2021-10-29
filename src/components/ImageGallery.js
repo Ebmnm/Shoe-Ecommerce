91f297667c937594270946883e4f6fa79adbeec0
mike <ebmnm77@gmail.com>
2021-10-29 15:49:55 -0400

    start

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -3,7 +3,7 @@ import "../styles/imagegallery.css"
 export default function ImageGallery() {
     return (
         <div>
-          <img alt="shoe" src="./images/image-product-1.jpg"/>
+          <img alt="shoe" src="../images/image-product-1.jpg"/>
         </div>
     )
 }
